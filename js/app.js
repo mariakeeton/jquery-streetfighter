af96ee23930de4fb785e5b8cb979038b435fe33f
mariakeeton <maria.keeton@gmail.com>
2015-01-13 21:36:30 -0500

    Added instructions and pose interactivity

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 $(document).ready(function() {
-     $(".ryu").mouseenter(function(){
-	  $(".ryu-still").hide();
-	  $(".ryu-ready").show();
-     })
-     .mouseleave(function() {
-	  $(".ryu-ready").hide();
-	  $(".ryu-still").show();
-     })
-     .mousedown(function() {
-	  playHadouken();
-	  $(".ryu-ready").hide();
-	  $(".ryu-throwing").show();
-	  $(".hadouken").finish().show()
-	  .animate(
-	  	{'left': '300px'},
-	  	500,function() {
-	  		$(this).hide();
-	  		$(this).css('left', '-202px');
-	  	});
-     })
-     .mouseup(function() {
-	  console.log("mouseup");
-	  $(".ryu-throwing").hide();
-	  $(".ryu-ready").show();
-     });
+	     $(".ryu").mouseenter(function(){
+			$(".ryu-still").hide();
+			$(".ryu-ready").show();
+    		 })
+     		.mouseleave(function() {
+				$(".ryu-ready").hide();
+				$(".ryu-still").show();
+    			 })
+     		.mousedown(function() {
+				playHadouken();
+				$(".ryu-ready").hide();
+				$(".ryu-throwing").show();
+				$(".hadouken").finish().show()
+				.animate(
+				  	{'left': '300px'},
+						500,function() {
+							$(this).hide();
+							$(this).css('left', '-202px');
+				});
+    			 })
+		     .mouseup(function() {
+				$(".ryu-throwing").hide();
+				$(".ryu-ready").show();
+		     });
+    
+}).bind('keydown', function (e){
+	if (e.keyCode == 88) {
+		$(".ryu-cool").show();
+		$(".ryu-ready").hide();
+		$(".ryu-still").hide();
+	}
+		
+}).bind('keyup', function (e){
+	if (e.keyCode == 88) {
+		$(".ryu-cool").hide();
+		$(".ryu-still").show();
+	}
 });
 function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
 }
